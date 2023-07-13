81102368cd7bf37323532d7d09702e1a404013e7
Pewaukee <misterkar6@gmail.com>
2023-07-13 11:10:02 -0700

    openai python

--- api/index.py
+++ api/index.py
@@ -1,13 +1,31 @@
-from flask import Flask, jsonify
+# flask imports
+from flask import Flask, jsonify, request
 from flask_cors import CORS # to call from other servers
 
-app = Flask(__name__)
-CORS(app)
+# import items from messenger.py
+from messenger import *
 
-@app.route("/api/python", methods=['GET'])
+app = Flask(__name__)
+CORS(app) # allow localhost:3000 to call the api
+
+@app.route("/api/python/test", methods=['GET'])
 def hello_world():
     return jsonify({
         'message': 'Bing bop, I am a bot'
+    })
+
+@app.route("/api/python/generate", methods=['GET'])
+def generate():
+    prompt = request.args.get('prompt')
+    print(prompt)
+    response_text = openai.ChatCompletion.create(
+        model='gpt-3.5-turbo',
+        messages=[{"role": "user", "content": prompt}],
+        temperature=2.0,
+        max_tokens=300,
+    )['choices'][0]['message']['content']
+    return jsonify({
+        'quote': response_text
     })
 
 if __name__ == '__main__':
