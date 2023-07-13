ed851a81d94e6dade50629ac66f10f95e84e3905
Pewaukee <misterkar6@gmail.com>
2023-07-13 08:25:48 -0700

    initial setup, printing string in client

--- api/index.py
+++ api/index.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, jsonify
+from flask_cors import CORS # to call from other servers
 
-@app.route("/api/python")
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/api/python", methods=['GET'])
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return jsonify({
+        'message': 'Bing bop, I am a bot'
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080) # remove debug mode in production
